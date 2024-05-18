485eda88ab5e9dcea802f7c8fe2d9dfce1fa6bd5
prabhakar1403 <sprabhakar1403@gmail.com>
2024-05-18 19:12:10 +0530

    Background changer project file initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [color, setColor] = useState(null);
+
+  const changeColor = (event) => {
+    let newColor = event.target.value;
+    setColor(newColor);
+  };
+  return (
+    <>
+    <div className='container' style={{backgroundColor: color}}>
+      <button
+      className='button'
+      onClick={changeColor}
+      value='purple'
+      >Violet
+      </button>
+      <button
+      className='button'
+      onClick={changeColor}
+      value='grey'
+      >Indigo
+      </button>
+      <button
+      className='button'
+      onClick={changeColor}
+      value='blue'
+      >Blue
+      </button>
+      <button
+      className='button'
+      onClick={changeColor}
+      value='green'
+      >Green
+      </button>
+      <button
+      className='button'
+      onClick={changeColor}
+      value='yellow'
+      >Yellow
+      </button>
+      <button
+      className='button'
+      onClick={changeColor}
+      value='orange'
+      >Orange
+      </button>
+      <button
+      className='button'
+      onClick={changeColor}
+      value='red'
+      >Red
+      </button>
+    </div>
+    </>
+  )
+}
+export default App
+
+
+// {color && <Color color={color} />}
